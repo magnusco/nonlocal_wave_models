e7335e25698a8c3c2bfcd991aeb45f92f4877042
magnusco <magnus.orke@lyse.net>
2021-06-05 23:25:21 +0200

    got dct to kind of work for fkdv

--- bifurcation_fkdv.py
+++ bifurcation_fkdv.py
@@ -3,57 +3,58 @@ from scipy import fft, optimize
 import matplotlib.pyplot as plt
 
 
-def func_guess(X, height, P):
-    return height * np.cos(((2 * np.pi) / P) * X)
+def func_guess(X_half, height, P):
+    return height * np.cos(((2 * np.pi) / P) * X_half)
 
 
 def bessel_symbol(Xi, s):
-    return np.power((1 + np.power(Xi, 2)), -s / 2)
+    return (1 + Xi ** 2) ** (-s / 2)
 
 
-def FDP_const_sol(wavespeed, integration_const):
-    return (wavespeed + np.sqrt(wavespeed ** 2 + 8 * integration_const)) / 4
+def even_half_to_full_P(half):
+    return np.concatenate((half, np.flip(half[1:-1])))
 
 
-def FKDV_system(sol, height, P, s, symbol_values):
-    phi, wavespeed = sol[:-1], sol[-1]
-    phi_coeffs = fft.rfft(phi)
-    scaled_phi_coeffs = fft.ifftshift(
-        np.multiply(fft.fftshift(phi_coeffs), symbol_values)
-    )
+def odd_half_to_full_P(half):
+    return np.concatenate((half, -np.flip(half[1:-1])))
 
-    equation_error = (
-        wavespeed * phi - (1 / 2) * np.power(phi, 2) - fft.irfft(scaled_phi_coeffs)
-    )
-    height_error = np.array([np.abs(height - phi[int(len(phi) / 2) + 1])])
-    return np.concatenate((equation_error, height_error))
+
+def FKDV_system(sol, height, P, s, symb_coeffs):
+    phi_half, mu = sol[:-1], sol[-1]
+    phi = np.concatenate((phi_half, np.flip(phi_half[1:-1])))
+
+    phi_coeffs = fft.dct(phi_half, type=1)
+    scaled_phi_coeffs = np.multiply(phi_coeffs, 2 * symb_coeffs)
+    smoothed_phi_half = fft.idct(scaled_phi_coeffs, type=1)
+    smoothed_phi = np.concatenate((smoothed_phi_half, np.flip(smoothed_phi_half[1:-1])))
+
+    eq_err = (mu * phi) - ((1 / 2) * phi ** 2) - smoothed_phi
+    height_err = np.array([np.abs(height - phi[int(len(phi) / 2) + 1])])
+    return np.concatenate((eq_err[: len(sol) - 1], height_err))
 
 
 if __name__ == "__main__":
     s = 0.5
     P = 2 * np.pi
-    N = 2 ** 8
-    X = np.linspace(-P / 2, P / 2, N, endpoint=False)
+    N = 2 ** 7
+    X_half = np.linspace(-P / 2, 0, N)
+    samples = 10
 
-    bifurcation_point = bessel_symbol((2 * np.pi) / P, s)
-    symbol_values = bessel_symbol(
-        np.array([(2 * np.pi * (k - N / 2)) / P for k in range(0, N)]), s
-    )
-    H = np.linspace(0, 0.3 * bifurcation_point, 30)
-    wavespeeds, max_heights = np.zeros(len(H)), np.zeros(len(H))
+    bif_point = bessel_symbol((2 * np.pi) / P, s)
+    symb_coeffs = bessel_symbol(np.array([(2 * np.pi * k) / P for k in range(0, N)]), s)
+    H = np.linspace(0, 0.2 * bif_point, samples)
+    wavespeeds, max_heights = np.zeros(samples), np.zeros(samples)
 
-    for i in range(0, len(H)):
-        initial_phi_guess = func_guess(X, H[i], P)
-        initial_wavespeed_guess = np.array([bifurcation_point])
+    for i in range(0, samples):
+        phi_guess = func_guess(X_half, H[i], P)
+        wavespeed_guess = np.array([0.5 * bif_point])
 
-        F = lambda sol: FKDV_system(sol, H[i], P, s, symbol_values)
-        solution = optimize.fsolve(
-            F, np.concatenate((initial_phi_guess, initial_wavespeed_guess))
-        )
+        F = lambda sol: FKDV_system(sol, H[i], P, s, symb_coeffs)
+        solution = optimize.fsolve(F, np.concatenate((phi_guess, wavespeed_guess)))
 
         wavespeeds[i] = solution[-1]
         max_heights[i] = np.max(solution[:-1])
-        plt.plot(X, solution[:-1], linewidth=0.7)
+        plt.plot(X_half, solution[:-1], "k", linewidth=0.7)
     plt.show()
-    plt.plot(wavespeeds, max_heights)
+    plt.plot(wavespeeds, max_heights, "k")
     plt.show()
