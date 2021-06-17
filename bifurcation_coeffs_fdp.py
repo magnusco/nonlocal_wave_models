be6c2750762c2bcc3b982eb16fc37d8ba6342cbc
magnusco <magnus.orke@lyse.net>
2021-06-17 12:32:21 +0200

    final plots used in thesis

--- bifurcation_coeffs_fdp.py
+++ bifurcation_coeffs_fdp.py
@@ -7,66 +7,67 @@ def bessel_symbol(Xi, s):
     return (1 + Xi ** 2) ** (-s / 2)
 
 
+def upper_bound_P(s):
+    return (2 * np.pi) / np.sqrt(3 ** (2 / s) - 1)
+
+
 def const_sol(mu, int_const):
     return (mu + np.sqrt(mu ** 2 + 8 * int_const)) / 4
 
 
 def const_sol_der(mu, int_const):
-    return (1 / 4) * (1 + np.divide(mu, np.sqrt(np.power(mu, 2) + 8 * int_const)))
+    return (1 + (mu / np.sqrt(mu ** 2 + 8 * int_const))) / 4
 
 
-def upper_bound_P(s):
-    return (2 * np.pi) / np.sqrt(3 ** (2 / s) - 1)
-
-
-def mu_2(Xi, s, symbol):
+def mu_2_simplified(Xi, s):
     m_1 = bessel_symbol(Xi, s)
     m_2 = bessel_symbol(2 * Xi, s)
-    return (1 / (1 - m_1)) - ((1 + 3 * m_2) / (8 * (m_1 - m_2)))
+    return 9 * m_1 + 3 * m_1 * m_2 - 11 * m_2 - 1
+
+
+def mu_2(Xi, s, int_const, bif_points):
+    m_1 = bessel_symbol(Xi, s)
+    m_2 = bessel_symbol(2 * Xi, s)
+    a = (1 + 3 * m_1) / (3 * const_sol(bif_points, int_const))
+    b = 1 / (
+        3 * m_1 * const_sol_der(bif_points, int_const)
+        + const_sol_der(bif_points, int_const)
+        - 1
+    )
+    c = (1 / (1 - m_1)) - (1 + 3 * m_2) / (8 * (m_1 - m_2))
+    return a * b * c
 
 
 if __name__ == "__main__":
-    s_samples = 20
-    dx = 1e-3
-    S = np.linspace(0.2, 0.9, s_samples)
+
+    dx = 1e-4
+    S = np.linspace(0.1, 0.9, 9)
     int_const = 1
-    s = 0.9
 
-    P = upper_bound_P(s)
-    P_values = np.arange(1e-1, P, dx)
-    Xi = (2 * np.pi) / P_values
+    for s in S:
+        P = upper_bound_P(s)
+        P_values = np.arange(1e-4, P, dx)
+        Xi = (2 * np.pi) / P_values
+        number_periods = len(P_values)
 
-    mu_2_values = mu_2(Xi, s, bessel_symbol)
-    plt.plot(P_values, mu_2_values, "k", linewidth=0.7)
-    plt.plot(P_values, np.zeros(len(P_values)), "k", linewidth=0.7)
-    plt.ylim([-5, 5])
-    plt.show()
+        # impl_bif_points = lambda mu: (mu - const_sol(mu, int_const)) - (
+        #     3 * const_sol(mu, int_const) * bessel_symbol(Xi, s)
+        # )
+        # bif_points = fsolve(impl_bif_points, np.ones(number_periods))
 
-    exit()
+        mu_2_values = mu_2_simplified(Xi, s)
+        # mu_2_values = mu_2(Xi, s, int_const, bif_points)
 
-    ############################################################################
-    min_mu_2_values = np.zeros(s_samples)
-    max_mu_2_values = np.zeros(s_samples)
-    for i in range(0, s_samples):
-        P = upper_bound_P(S[i])
-        Xi = (2 * np.pi) / np.arange(1e-5, P, dx)
+        plt.plot(P_values, mu_2_values, "k", linewidth=0.7)
+        plt.plot(P_values, np.zeros(number_periods), "k", linewidth=0.7)
 
-        impl_bif_points = lambda mu: (mu - const_sol(mu, int_const)) - (
-            3 * const_sol(mu, int_const) * bessel_symbol(Xi, S[i])
-        )
-
-        bif_points = fsolve(impl_bif_points, np.ones(len(Xi)))
-        const_sols = const_sol(bif_points, int_const)
-        const_sol_ders = const_sol_der(bif_points, int_const)
-
-        mu_2_values = mu_2(
-            Xi, S[i], bif_points, const_sols, const_sol_ders, bessel_symbol
-        )
-        min_mu_2_values[i] = np.min(np.abs(mu_2_values))
-
-    plt.xlabel(r"$s$")
-    plt.ylabel(r"$\min\ |\mu_2|$")
-    plt.plot(S, min_mu_2_values, "k")
+    plt.xlabel(r"$P$")
+    plt.ylabel(r"$\mu_2$")
+    plt.ylim([-1.25, 0.5])
+    plt.text(-0.01, -0.65, r"$s = $" + str(0.1))
+    plt.text(0.9, -0.17, r"$s = $" + str(0.6))
+    plt.text(1.8, 0.2, r"$s = $" + str(0.9))
+    plt.text(0.2, 0.02, r"$\mu_2 = 0$")
     plt.savefig("mu_2_fdp.png")
     plt.show()
     plt.close()
